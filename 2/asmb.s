8549fdf35599ba24da2e9734fa2bf38675a51da6
Jakob Gruber <jakob.gruber@gmail.com>
2012-03-05 19:43:52 +0100

    Further optimization, retab

--- 2/asmb.s
+++ 2/asmb.s
@@ -1,22 +1,16 @@
-	.file	"asmb.c"
-	.text
-	.globl	asmb
-	.type	asmb, @function
+    .text
+    .globl  asmb
+    .type   asmb, @function
+
 asmb:
-.LFB0:
-	.cfi_startproc
-# x[]: rdi, n: rsi
-	dec 	%rsi                    # n = n - 1
-	js      out                     # if (n < 0) goto L1
-    clc                             # clear carry
+    .cfi_startproc              # rdi: x, rsi: n
+    cmp     $0, %rsi            # note: clears carry
+    jle     out                 # if (n <= 0) return
+    mov     %rsi, %rcx          # i = n
 loop:
-    rcrq    (%rdi,%rsi,8)           # x[n] = x[n] >> 1, save carry
-	dec 	%rsi                    # n = n - 1
-	jns	    loop                    # if (n >= 0) goto loop
+    rcrq    -8(%rdi, %rcx, 8)   # rotate x[i - 1] right through carry
+    loop    loop                # if (--i) != 0 goto loop
 out:
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	asmb, .-asmb
-	.ident	"GCC: (GNU) 4.6.2 20120120 (prerelease)"
-	.section	.note.GNU-stack,"",@progbits
+    ret
+    .cfi_endproc
+
